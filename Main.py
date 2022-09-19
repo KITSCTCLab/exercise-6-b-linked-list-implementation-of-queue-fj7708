b28c38dcbe55069d13fbcace833244aa538af65b
fj7708 <110383912+fj7708@users.noreply.github.com>
2022-09-19 15:25:05 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,36 +1,48 @@
 class Node:
-def	init (self, data): self.data = data self.next = None
+  def __init__(self, data):
+    self.data = data
+    self.next = None
 
 
+class Queue:
+  def __init__(self):
+    self.head = None
+    self.last = None
 
-class Stack:
-def	__init__ (self): self.head = None
+  def enqueue(self, data) -> None:
+    # Write your code here
+    temp = Node(data)
+    if(self.head == None):
+      self.head = self.last = temp
+    else:
+      self.last.next = temp
+      self.last = temp
 
-def push(self, data) -> None: # Write your code here new_node = Node(data) new_node.next = self.head self.head = new_node
+  def dequeue(self) -> None:
+    # Write your code here
+     if(self.head != None):
+       self.head = self.head.next
 
-def pop(self) -> None:
-# Write your code here if(self.head != None):
-temp = self.head self.head = temp.next temp.next = None
-
-def status(self):
-"""
-It prints all the elements of stack.
- 
-"""
-# Write your code here if(self.head != None):
-temp = self.head while(temp.next != None): print(temp.data, end = "=>") temp = temp.next
-else:
-print(temp.data,"None", sep="=>") else:
-print("None")
+  def status(self) -> None:
+    # Write your code here
+     temp = self.head
+     while(temp != None):
+       print(temp.data, end= "=>")
+       temp = temp.next
+     print("None")
+     
 
 
-
-
-# Do not change the following code stack = Stack()
+# Do not change the following code
+queue = Queue()
 operations = []
-for specific_operation in input().split(','): operations.append(specific_operation.strip())
+for specific_operation in input().split(','):
+    operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
-for i in range(len(operations)): if operations[i] == "push": stack.push(int(data[i]))
-elif operations[i] == "pop": stack.pop()
-stack.status()
+for i in range(len(operations)):
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
